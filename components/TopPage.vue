bf494f2666a64837ff938dea5ead9422c9e216b1
chitose87 <chitose87@gmail.com>
2020-12-04 18:20:04 +0900

    update

---
+++ components/TopPage.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+
+</template>
+<script lang="ts">
+  import {Component, Vue} from "~/node_modules/nuxt-property-decorator";
+
+  @Component({
+    components: {}
+  })
+  export default class TopPage extends Vue {
+    mounted() {
+    }
+  }
+</script>
+<style>
+</style>
